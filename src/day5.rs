6fbee06aa485e346e530753b33ca4f6796e38758
ashishn <nagav.ashish@gmail.com>
2015-12-28 14:32:58 -0800

    day3: working

---
+++ src/day5.rs
@@ -0,0 +1,20 @@
+use std::io::{self,BufRead};
+
+
+fn isNice(line: &str){
+
+
+
+}
+
+
+pub fn run(){
+    let stdin =  io::stdin();  
+    let count =0;
+    for line in stdin.lock().lines(){
+        if isNice(&line){
+        }
+        else{
+        }
+    }
+}
