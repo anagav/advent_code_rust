7266d0c7dbeb5a4f55e45e73920a64ba326c235e
ashishn <nagav.ashish@gmail.com>
2016-01-02 12:40:42 -0800

    [Day 2.2] solved

--- src/bin/day2.rs
+++ src/bin/day2.rs
@@ -5,17 +5,14 @@ use std::io::{self,BufRead};
 
 fn calculate_paper_needed(v: &mut Vec<i32>) -> i32 {
     let surface_area = 2*(v[0]*v[1] + v[1]*v[2] + v[2]*v[0]);
-    //println!("{}",surface_area);
     v.sort();
     let additional_area = v[0]*v[1];
     return surface_area + additional_area;
 }
 
-fn ribbon_feed(v: &mut Vec<i32>) -> i32{
+fn ribbon_feed(v: &Vec<i32>) -> i32{
     (2*(v[0]+v[1])) + (v[0]*v[1]*v[2])
 }
-
-
 
 pub fn run(){
     let stdin =  io::stdin();
@@ -27,41 +24,13 @@ pub fn run(){
         }
     ).filter_map(
         |mut vector|{
-            Some((calculate_paper_needed(&mut vector),ribbon_feed(&mut vector)))
+            Some((calculate_paper_needed(&mut vector),ribbon_feed(&vector)))
         }
     ).fold((0,0),|(sum,sum2),(element,element2)|{
         (sum+element,sum2+element2)
     });
-
     println!("problem-1 {:?}", paper_needed);
     println!("problem-2 {:?}", paper_needed_2);
-
-
-    // let result:Vec<_> = stdin.lock().lines().map(
-    //     |x| x.unwrap().split("x").map(
-    //         |y| y.parse::<i32>().unwrap()
-    //     ).collect::<Vec<i32>>()
-    // ).map(|in_vec|{
-    //     calculate_paper_needed(&mut in_vec)
-    // });
-
-    //println!("{:?}", result);
-
-
-
-    //
-    // {
-    //     //println!("{}",line);
-    //     let input = line.unwrap();
-    //     let mut in_line:Vec<_> = input.split("x").map(
-    //         |x| x.parse::<i32>().unwrap()
-    //     )
-    //
-    //     println!("{:?}", in_line);
-    //
-    //     paper_needed += calculate_paper_needed(&mut in_line);
-    // }
-    // println!("{}",paper_needed);
 }
 
 fn main() {
